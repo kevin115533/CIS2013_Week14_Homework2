48194b3f8c844f5e2d32ce3f9cad1693fcb2f549
kevin115533 <35547586+kevin115533@users.noreply.github.com>
2018-04-26 14:13:33 -0500

    Week 14 homework 2
    add to list function now supports a string with spaces

---
+++ Main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include "ToDo.h"
+using namespace std;
+
+int main() {
+	char next = 'y';
+	int len = 0;
+	string action;
+	
+	cout << "How long do you want your list? ";
+	cin >> len;
+
+	ToDo list(len);
+
+	while (next != 'n') {
+		cout << "Add to list (a)" << endl;
+		cout << "Done list item (d)" << endl;
+		cout << "Print list (p)" << endl;
+		cout << "Exit list (x)" << endl;
+		cout << "What do you want to do: ";
+		cin >> next;
+
+
+		switch (next) {
+		case 'a':
+			cout << "What do you need to do? ";
+			cin.ignore();
+			getline(cin, action);
+			list.add(action);
+			break;
+		case 'd':  
+			list.done();
+			break;
+		case 'p':  
+			list.print();
+			break;
+		case'x':   
+			break;
+
+		}
+	}
+}
---
+++ ToDo.cpp
@@ -0,0 +1,35 @@
+#include "ToDo.h"
+
+using namespace std;
+
+ToDo::ToDo() {
+	ToDo(5);
+}
+
+ToDo::ToDo(int len) {
+	length = len;
+	list = new string[length];
+}
+
+ToDo::~ToDo() {
+	delete[] list;
+}
+
+void ToDo::add(string item) {
+	if (next < length) {
+		list[next] = item;
+		next++;
+	}
+}
+
+void ToDo::done() {
+	next--;
+	list[next] = "";
+}
+
+void ToDo::print() {
+	for (int i = 0; i < next; i++) {
+		cout << i+1 << " " << list[i] << endl;
+	}
+
+}
